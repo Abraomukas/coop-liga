680aced2cf1928e1f54e0702125321639b1f8455
Abraomukas <abraomukas@gmail.com>
2024-09-24 23:50:46 +0200

    wip: add index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const spinnerSizing = { width: '3rem', height: '3rem' };
+
+const spinnerFallback = (
+	<div className='d-flex justify-content-center mt-3'>
+		<div className='spinner-border' style={spinnerSizing} role='status'>
+			<span className='visually-hidden'>Loading...</span>
+		</div>
+	</div>
+);
+
+/**
+ ** PAGES
+ */
+import Error from './pages/Error';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Main />,
+		errorElement: <Error />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.Suspense fallback={spinnerFallback}>
+		<RouterProvider router={router} />
+	</React.Suspense>
 );
