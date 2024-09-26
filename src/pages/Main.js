f4749f7952b41afac2dfdd89fea7aecc9cdf47ef
Abraomukas <abraomukas@gmail.com>
2024-09-26 22:39:12 +0200

    wip: add Main page

---
+++ src/pages/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+function Main() {
+	return (
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			{/* SECTIONS */}
+			{sections.map((SectionComponent, index) => (
+				<section key={index}>
+					{/* */}
+					{/* */}
+					{/* */}
+				</section>
+			))}
+
+			<Footer />
+		</div>
+	);
+}
+
+export default Main;
