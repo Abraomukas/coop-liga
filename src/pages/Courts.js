6f8b88335918f3bf9f36ab43d5f4f24b21dc05ed
Abraomukas <abraomukas@gmail.com>
2024-09-28 09:45:00 +0200

    wip: add About page

---
+++ src/pages/Courts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function Courts() {
+	return (
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			{/* */}
+
+			<Footer />
+		</div>
+	);
+}
