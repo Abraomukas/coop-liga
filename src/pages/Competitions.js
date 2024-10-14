138a08a73d7a5b6ef6f85ded846213bde6c4ebd9
Abraomukas <abraomukas@gmail.com>
2024-10-14 07:15:00 +0200

    feat: add teams from group 1

--- src/pages/Competitions.js
+++ src/pages/Competitions.js
@@ -5,6 +5,14 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function Competitions() {
+	const competitions = [];
+	const teams = [
+		{ name: 'Estu 97', group: 1 },
+		{ name: 'Jagger Mommiers', group: 1 },
+		{ name: 'La Lima Mecánica', group: 1 },
+		{ name: 'Airballs', group: 1 },
+	];
+
 	return (
 		<div style={{ position: 'relative', minHeight: '100vh' }}>
 			<Navbar />
