c8e68c7807fbc835b5226d9444ec99629c255c42
Abraomukas <abraomukas@gmail.com>
2024-10-25 07:25:00 +0200

    feat: add Results page

---
+++ src/pages/Results.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//* COMPONENTS
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function Results() {
+	return (
+		<div style={{ position: 'relative', minHeight: '100vh' }}>
+			<Navbar />
+
+			{/* */}
+			<div style={{ position: 'relative', minHeight: '100vh' }}></div>
+
+			<Footer />
+		</div>
+	);
+}
